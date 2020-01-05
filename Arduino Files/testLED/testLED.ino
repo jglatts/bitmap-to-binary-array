759b0b9cdbf9bf64101406ff71d6454f62ff1553
John Glatts <john@z-axiscc.com>
2020-01-05 08:59:41 -0500

    Update testLED.ino

--- Arduino Files/testLED/testLED.ino
+++ Arduino Files/testLED/testLED.ino
@@ -1,39 +1,34 @@
+/*
+  Get the overwrite text working
+  - have to clear just a small section of the LED
+*/
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "Bit_Maps.h"
 
-#define SCREEN_WIDTH  128 
-#define SCREEN_HEIGHT 32 
-#define OLED_RESET    -1 
-#define XPOS          0 
-#define YPOS          1
-#define DELTAY        2
-// smiley is 40x40
-// also try 80x20
-#define LOGO_HEIGHT   40
-#define LOGO_WIDTH    40
-
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+/* Time Counters */
+int start = millis(), elapsed_time;
 
 void setup() {
   Serial.begin(9600);
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for (;;); 
+    for (;;);
   }
   displaySplash();
+  drawBitMap();
 }
 
 void loop() {
-  testdrawbitmap();
-  testFonts();
+  displayTime();
 }
 
 void displaySplash() {
   display.clearDisplay();
-  delay(1000);
   testdrawcircle();
   delay(2500);
   display.clearDisplay();
@@ -66,28 +61,76 @@ void testscrolltext(void) {
 
 void testdrawcircle(void) {
   display.clearDisplay();
-  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, SSD1306_WHITE);
+  for (int16_t i = 0; i < max(display.width(), display.height()) / 2; i += 2) {
+    display.drawCircle(display.width() / 2, display.height() / 2, i, SSD1306_WHITE);
     display.display();
+    delay(1);
+  }
+  delay(1000);
+}
+
+void testFonts() {
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(95, 3);
+  display.println(F("Kush"));
+  display.setCursor(95, 10);
+  display.println(F("Foo.."));
+  display.display();
+}
+
+void displayTime() {
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(93, 18);
+  display.print(getMins());
+  display.print(":");
+  display.print(getSecs());
+  display.display();
+  delay(300);
+  drawRect();
+}
+
+void clearLine() {
+  display.drawFastHLine(30, 14, 100, SSD1306_BLACK);
+  display.drawFastHLine(31, 14, 100, SSD1306_BLACK);
+  display.drawFastHLine(32, 14, 100, SSD1306_BLACK);
+  display.drawFastHLine(33, 14, 100, SSD1306_BLACK);
+  display.display();
+}
+
+void drawBitMap() {
+  int xVal, yVal;
+  display.clearDisplay();
+  xVal = (display.width() - KUSH_WIDTH) / 2;
+  yVal = (display.height() - KUSH_HEIGHT) / 2;
+  display.drawBitmap(xVal, yVal, kush, KUSH_WIDTH, KUSH_HEIGHT, 1);
+  display.display();
+}
+
+int getMins() {
+  elapsed_time = millis() - start;
+  return int(elapsed_time / 60000);
+}
+
+int getSecs() {
+  int mod = elapsed_time % 60000;
+  return int(mod / 1000);
+}
+
+void testdrawrect(void) {
+  display.clearDisplay();
+  for (int16_t i = 0; i < display.height() / 2; i += 2) {
+    display.drawRect(i, i, display.width() - 2 * i, display.height() - 2 * i, SSD1306_WHITE);
+    display.display(); // Update screen with each newly-drawn rectangle
     delay(1);
   }
   delay(2000);
 }
 
-void testFonts() {
-  //display.clearDisplay();
-  display.setTextSize(1); 
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(95, 0);
-  display.println(F("Ayoo"));
-  display.setCursor(95, 15);
-  display.println(F("Foo!"));
-  display.display();      // Show initial text
-}
-
-void testdrawbitmap(void) {
-  //display.clearDisplay();
-  int xVal = (display.width() - LOGO_WIDTH) / 2;
-  display.drawBitmap(xVal, 0, smiley, LOGO_WIDTH, LOGO_HEIGHT, 1);
+void drawRect(void) {
+  int xVal = display.width() - 2;
+  int radius = display.height() / 4;
+  display.fillRoundRect(90, 18, xVal, 10, radius, SSD1306_BLACK);
   display.display();
 }
