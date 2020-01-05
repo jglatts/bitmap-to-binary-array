1c0352dc0b3308432b58903f13d8ddd9cf87f5da
John Glatts <john@z-axiscc.com>
2020-01-05 17:20:14 -0500

    Update Bmp.java

--- Java Files/bmp/src/bmp/Bmp.java
+++ Java Files/bmp/src/bmp/Bmp.java
@@ -1,34 +1,43 @@
+ /**
+ *  Java Program to convert a binary image into a binary array
+ *  Used to display images on LCD screens, etc..
+ * 
+ *  @author: John G.
+ */
 package bmp;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 
 public class Bmp {
-
+    
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner input = new Scanner(new File("t.txt"));
-        while (input.hasNextLine()) {
-            String s = input.nextLine();
-            //printCommas(s);
-            printBinArray(s);
-        }
+        Scanner img = new Scanner(new File("b.txt"));
+        String f = getFileName();
+        PrintStream outPut = new PrintStream(new File(f));
+        while (img.hasNextLine()) 
+            writeBinaryFile(img.nextLine(), outPut);
+        System.out.println("The Binary Array is saved at: " + f);
     }
     
-    public static void printCommas(String s) {
+    public static String getFileName() {
+        Scanner f = new Scanner(System.in);
+        System.out.print("Enter a file name to save the Binary Array to: ");
+        return f.nextLine();
+    }
+    
+    public static void writeBinaryFile(String s, PrintStream p) {
         int count = 0;
         for (int i = 0; i < s.length(); ++i) {
             count++;
             if (count == 8) {
-                System.out.println(s.substring(0, count));
-                printCommas(s.substring(count));
+                p.println("B" + s.substring(0, count) + ",");
+                writeBinaryFile(s.substring(count), p);
             }
-        }
+        }    
     }
     
-    public static void printBinArray(String s) {
-        System.out.println("B" + s + ",");
-    }
-
 }    
